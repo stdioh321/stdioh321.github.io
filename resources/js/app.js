8aecc3ebe292e551108fd935a14cad4333feb58d
John <diaslinoh@gmail.com>
2016-04-22 19:40:03 -0300

    asdas
    assadas

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,13 @@
-var urlBase = "http://api.worldweatheronline.com/premium/v1/";
-var url = "http://api.worldweatheronline.com/premium/v1/marine.ashx?key=f2ae69adff674902a3c13610160504&format=json&q=";
-var key = "f2ae69adff674902a3c13610160504";
+//http://api.worldweatheronline.com/free/v1/marine.ashx?key=d7pesx54engabthjcznhanfa&format=json&q=-25.8685,-48.5589
+//d7pesx54engabthjcznhanfa
+//a71c81ee86b5409d9f331200160504
+var urlBase = "https://api.worldweatheronline.com/premium/v1/";
 
-var urlBaseMarine = "marine.ashx?key=" + key + "&format=json&tp=1&q=";
+var url = "http://api.worldweatheronline.com/premium/v1/marine.ashx?key=f2ae69adff674902a3c13610160504&format=json&q=";
+//Premium Eterna var key = "f2ae69adff674902a3c13610160504";
+var key = "a71c81ee86b5409d9f331200160504";
+
+var urlBaseMarine = "marine.ashx?key=" + key + "&lang=pt&format=json&tp=1&q=";
 var urlBaseWeather = "weather.ashx?key=" + key + "&lang=pt&showmap=yes&format=json&tp=1&q="
 
 
