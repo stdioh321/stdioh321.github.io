8e3273c7fd92b37282751de869d9dec69acf9eec
John <diaslinoh@gmail.com>
2016-04-22 23:58:31 -0300

    Modal
    moa

--- resources/js/filters/filters.js
+++ resources/js/filters/filters.js
@@ -1,14 +1,14 @@
 app.filter('toHour', function () {
     return function (input) {
-
+        if(input == undefined){
+            input = "";
+        }
         if (input.length == 4) {
             input = input.substring(0, 2) + "h";
-        } else
-        if (input.length == 3) {
+        } else if (input.length == 3) {
             input = "0" + input.substring(0, 1) + "h";
-        } else
-
-        if (input.length == 1) {
+        }
+        else if (input.length == 1) {
             input = "00" + "h";
         }
         return input;
