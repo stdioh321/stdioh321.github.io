4a6cea072e98f60c8b1f6acbea16f101a49c5a13
Henrique <diaslinoh@gmail.com>
2019-06-19 15:44:13 -0300

    Sendmail feature added

---
+++ php/sendmail.php
@@ -0,0 +1,11 @@
+<?php
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+
+	$msg = $name.",\r\n". $_POST['message'];
+	$to = "diaslinoh@gmail.com";
+	$subject = "Mensagem Portfolio";
+	$headers = "From: ". $email;
+
+	mail($to,$subject,$msg,$headers);
+?>
